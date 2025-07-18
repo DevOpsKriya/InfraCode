cc0aa92db614684649fb8a2e3c6cfc71c203d075
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-18 23:24:24 +0530

    v3 naya rg bnaya

--- rg.tf
+++ rg.tf
@@ -5,6 +5,12 @@ resource "azurerm_resource_group" "practice" {
 }
 
 resource "azurerm_resource_group" "practice2" {
+  name     = "rg-practice2"
+  location = "East US"
+  
+}
+
+resource "azurerm_resource_group" "practice2" {
   name     = "rg-practice3"
   location = "East US"
   
